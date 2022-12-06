2e65c9ee170b1c5efe45d63d3ef878576c5129a8
Worseq <112353977+Worseq@users.noreply.github.com>
2022-12-07 00:11:15 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,20 +1,20 @@
 export default function Tos() {
   return (
     <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Hizmet şartları</h2>
+     <h2 className="mt-4 text-white font-bold text-3xl">Hizmet Şartları</h2>
       <p className="text-sm text-white/30 mb-1">Son Güncelleme: 07/12/2022 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
 <p>⊳ Her discord sunucusu MYRP botunu ücretsiz olarak kullanabilir, ücret karşılığında satılması veya pazarlanması kesinlikle yasaktır.</p>
 <div className="py-3"></div>
 <p>⊳ Bot komutlarını bota zarar vermek için kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+<p>⊳ Diğer sunuculara zarar vermek için açık kaynak kodları kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ Diğer platformlar aracılığıyla açık kaynak kodlarını satamaz veya pazarlayamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
+<p>⊳ Botu kullanırken diğer kullanıcılara zarar verecek şekilde troll amaçlı kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot at certain intervals.</p>
+<p>⊳ Belirli discord bot komutlarını kullanabilmek için belirli aralıklarla: <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">MYRP</a> botuna oy vermeniz gerekmektedir.</p>
 </div>
     </div>
   );
