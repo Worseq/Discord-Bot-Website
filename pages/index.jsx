1fe92da1318f0dec8707336edf142e1c41d45b47
Worseq <112353977+Worseq@users.noreply.github.com>
2022-12-06 23:55:22 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,8 +12,7 @@ export default function Index() {
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
           MYRoleplay
         </p>
-        <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       How about moderating your own server with Astra bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
+        <p className="animateHeader text-white text-opacity-50 text-center mt-5"> 
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
